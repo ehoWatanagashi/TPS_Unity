7a83d7838483d99aad60c1064378f3f67e68ce3f
Artem Pleshkun <asperatus@inbox.ru>
2020-10-03 21:07:05 +0300

    Add mines spawner, mines physics

---
+++ Assets/Scripts/MinePhysics.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinePhysics : MonoBehaviour
+{
+
+    [SerializeField] private float _explosionRadius = 20.0F;
+    [SerializeField] private float _explosionPower = 200.0F;
+    [SerializeField] private GameObject _explosion;
+
+
+    void Start()
+    {
+        StartCoroutine(ExplosionBomb());   
+    }
+
+    IEnumerator ExplosionBomb()
+    {
+        //delay 3 sec
+        yield return new WaitForSeconds(3);
+        StartCoroutine(ExplosionBomb());
+
+        Vector3 _explosionPosition = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(_explosionPosition, _explosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody _RigidBody = hit.GetComponent<Rigidbody>();
+            if (_RigidBody != null)
+                _RigidBody.AddExplosionForce(_explosionPower, _explosionPosition, _explosionRadius, 3.0F);
+        }
+        //add visual effect (bigExplosion) and destroy mine
+        Instantiate(_explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/MineSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MineSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _mine;
+    [SerializeField] private Transform _mineSpawnPoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(_mine, _mineSpawnPoint.position, _mineSpawnPoint.rotation);
+        }
+    }
+}
